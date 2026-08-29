c7d2fe8a4fe79fa77e00d999730243fe79f5d4fb
Hendra <erhanhendragianpermana@gmail.com>
2026-08-29 12:15:31 +0700

    Jobsheet-01

--- Jobsheet1/OOP.java
+++ Jobsheet1/OOP.java
@@ -1,32 +1,33 @@
 package Jobsheet1;
 
 class Product {
-
-    private final String iteName;
+    private final String itemName;
     private final float price;
 
-    public Product (String itemName, float price) {
-        this.iteName = itemName;
+    public Product(String itemName, float price) {
+        this.itemName = itemName;
         this.price = price;
     }
 
-    public float total (int quantity) {
+    public float total(int quantity) {
         return price * quantity;
     }
 
     public String getName() {
-        return iteName;
+        return itemName;
     }
 }
 
 public class OOP {
-
     public static void main(String[] args) {
-
-        Product product1 = new Product("Coffee", 12000.0f);
-        Product product2 = new Product("Sugar", 3000.0f);
+        Product product1 = new Product("Coffee", 120000.0f);
+        Product product2 = new Product("Sugar", 30000.0f);
+        Product product3 = new Product("Milk", 25000.0f);
+        Product product4 = new Product("Tea", 15000.0f);
 
         System.out.println(product1.getName() + ": " + product1.total(3));
         System.out.println(product2.getName() + ": " + product2.total(1));
-        }
+        System.out.println(product3.getName() + ": " + product3.total(2));
+        System.out.println(product4.getName() + ": " + product4.total(4));
     }
+}
--- Jobsheet1/Procedural.java
+++ Jobsheet1/Procedural.java
@@ -2,20 +2,29 @@ package Jobsheet1;
 
 public class Procedural {
     public static void main(String[] args) {
-
-        String itemName1 = "Coffe";
-        float price1 = 12000.0f;
+        String itemName1 = "Coffee";
+        float price1 = 120000.0f;
         int quantity1 = 3;
 
         String itemName2 = "Sugar";
-        float price2 = 3000.0f;
+        float price2 = 30000.0f;
         int quantity2 = 1;
+
+        String itemName3 = "Milk";
+        float price3 = 25000.0f;
+        int quantity3 = 2;
+
+        String itemName4 = "Tea";
+        float price4 = 15000.0f;
+        int quantity4 = 4;
 
         System.out.println(itemName1 + ": " + total(price1, quantity1));
         System.out.println(itemName2 + ": " + total(price2, quantity2));
+        System.out.println(itemName3 + ": " + total(price3, quantity3));
+        System.out.println(itemName4 + ": " + total(price4, quantity4));
     }
 
-    static float total (float price, int quantity) {
+    static float total(float price, int quantity) {
         return price * quantity;
     }
 }
